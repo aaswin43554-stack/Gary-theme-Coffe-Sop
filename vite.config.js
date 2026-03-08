0270c2e3463b9e784e2b5ca9ae3f1ffab860498a
aaswin43554-stack <aaswin43554@gmail.com>
2026-03-08 16:01:05 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    allowedHosts: ['www.oneestate.coffee', 'oneestate.coffee']
+  server: {
+    allowedHosts: [
+      'gary-theme-coffe-sop.onrender.com'
+    ]
   }
 })
